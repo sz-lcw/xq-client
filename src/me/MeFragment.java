3df04765584f5d1897a087a2aeb9259f8aeed167
Danieldachao <2483720414@qq.com>
2015-05-03 20:24:40 +0800

    me修改

--- src/me/MeFragment.java
+++ src/me/MeFragment.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.Window;
 import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.Button;
@@ -17,18 +18,23 @@ public class MeFragment extends Fragment
 {
 	private Button settingButton;
 	private Button persionSettingButton;
-	
+	private Button collectButton;
 	@Override
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState)
 	
 	{       
+		
+		 
 		 final   View view=inflater.inflate(R.layout.me, container, false);
 		    settingButton=(Button) view.findViewById(R.id.tab04_setting);
 		    settingButton();
 		    
 		    persionSettingButton=(Button) view.findViewById(R.id.tab04_persionSetting);
 		    persionSettingButton();
+		    
+		    collectButton=(Button)view.findViewById(R.id.collect);
+		    collectButton();
 		    
             return view;
             
@@ -39,6 +45,7 @@ public class MeFragment extends Fragment
 		persionSettingButton.setOnClickListener(new OnClickListener() {	
 			@Override
 			public void onClick(View arg0) {
+				
 			    Intent intent =new Intent();
 			    intent.setClass(getActivity(), Me_Persion_Setting.class);
 			    startActivity(intent);
@@ -57,6 +64,19 @@ public class MeFragment extends Fragment
 				startActivity(intent);
 			}
 		});
+	}
+	private void collectButton(){
+		collectButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				Intent intent=new Intent();
+				intent.setClass(getActivity(), Setting.class);
+				startActivity(intent);
+			}
+		});
+		
 	}
 
 }
